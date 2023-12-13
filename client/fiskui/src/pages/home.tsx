cf052980dd951f597ded1bc102c7bb7a8ef09d1a
Eirik Kjeken <Eirik.Kjeken@sintef.no>
2023-12-13 15:58:54 +0100

    use a ref to keep track of polling timer

--- client/fiskui/src/pages/home.tsx
+++ client/fiskui/src/pages/home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FiskologiskOptimizationForm } from "../components/fiskologiskOptimizationForm"
 import { FiskologiskResults } from "../components/fiskologiskResults"
 import axios from "axios";
@@ -13,11 +13,12 @@ export enum ResultState {
 export const HomePage = () => {
     const [resultState, setResultState] = useState<ResultState>(ResultState.Loading);
     const [resultData, setResultData] = useState<any | null>(null)
+    const activePollingTimer = useRef<NodeJS.Timeout>(); // In case we exit or something strange happens while waiting for a polling call or whatever
 
     useEffect(() => {
         if (resultState === ResultState.Loading) {
-            let timerId = setTimeout(fetchData, 1000);
-            return () => clearInterval(timerId);
+            activePollingTimer.current = setTimeout(fetchData, 1000);
+            return () => clearInterval(activePollingTimer.current);
         }
     });
 
@@ -30,7 +31,7 @@ export const HomePage = () => {
         // TODO: replace this with proper endpoint
         axios.get(`https://api.chucknorris.io/jokes/random`).then((response) => {
             if (response.data["status"] === "computing") {
-                setTimeout(fetchData, 1000)
+                activePollingTimer.current = setTimeout(fetchData, 1000)
             }
             else if (response.data["status"] === "failed") {
                 setResultState(ResultState.Infeasible);
