9f8ad58e441ab5e0f7c24c81634fa07a48569087
Eirik Kjeken <Eirik.Kjeken@sintef.no>
2023-12-19 16:46:36 +0100

    Try to get spinner to work

--- public/app.js
+++ public/app.js
@@ -9,11 +9,11 @@ const height = 600 - margin.top - margin.bottom;
 const svg = d3.select("body")
     .append("svg")
     .attr("viewBox", [0, 0, width, height])
-    // .attr("width", width + margin.left + margin.right + legendWidth)
-    // .attr("height", height + margin.top + 2*margin.bottom + heightBiomass + heightBiomassTitle)
-    // .append("g")
-    // .attr("transform",
-    // "translate(" + margin.left + "," + margin.top + ")");
+    .attr("width", width + margin.left + margin.right + legendWidth)
+    .attr("height", height + margin.top + 2*margin.bottom + heightBiomass + heightBiomassTitle)
+    .append("g")
+    .attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");
 
 // This stateless component renders a static "wheel" made of circles,
 // and rotates it depending on the value of props.angle.
